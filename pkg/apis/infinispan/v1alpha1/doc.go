52cf92a69d111cf8d9e566575fa027604be09113
Nandor Kracser <bonifaido@gmail.com>
2018-04-13 14:03:08 +0200

    initial import

---
+++ pkg/apis/infinispan/v1alpha1/doc.go
@@ -0,0 +1,3 @@
+// +k8s:deepcopy-gen=package
+// +groupName=infinispan.banzaicloud.com
+package v1alpha1
---
+++ pkg/apis/infinispan/v1alpha1/types.go
@@ -0,0 +1,29 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type InfinispanList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []Infinispan `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type Infinispan struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              InfinispanSpec   `json:"spec"`
+	Status            InfinispanStatus `json:"status,omitempty"`
+}
+
+type InfinispanSpec struct {
+	// Fill me
+}
+type InfinispanStatus struct {
+	// Fill me
+}
